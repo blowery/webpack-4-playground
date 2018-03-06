1ac4292fa3a89baa58e10f7a5c6a173f141ad630
Ben Lowery <ben.lowery@automattic.com>
2018-03-06 11:57:00 -0800

    add in some chunk cycles

--- src/b.js
+++ src/b.js
@@ -2,10 +2,10 @@ import { times } from 'lodash';
 import * as React from 'react';
 
 export default function Placeholders(num) {
-	//return import(/* webpackChunkName: "a" */ './a').then(function(a) {
-	return times(
-		num,
-		React.createElement('div', { className: 'placeholder' }, 'hold on, for one more day'),
-	);
-	//});
+	return import(/* webpackChunkName: "a" */ './a').then(function(a) {
+		return times(
+			num,
+			React.createElement('div', { className: 'placeholder' }, 'hold on, for one more day'),
+		);
+	});
 }
